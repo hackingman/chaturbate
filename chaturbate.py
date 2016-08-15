81b4bb9c813ac813fe8d195284a2b90e61da7027
Pedro de Oliveira <falsovsky@gmail.com>
2016-08-15 20:42:25 +0100

    b

--- chaturbate.py
+++ chaturbate.py
@@ -70,20 +70,21 @@ class Chaturbate(object):
         self.password = self.config_parser.get('User', 'password')
 
     @staticmethod
-    def get_human_size(nbytes):
-        """Returns file size in a human readable format
+    def get_human_size(bytes):
+        """Returns file size in a human readable format.
 
-        :param int nbytes: File size in bytes
+        :param int bytes: File size in bytes
         :return: Pretty file size
         :rtype: str
         """
         suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-        if nbytes == 0: return '0 B'
+        if bytes == 0:
+            return '0 B'
         i = 0
-        while nbytes >= 1024 and i < len(suffixes)-1:
-            nbytes /= 1024.
+        while bytes >= 1024 and i < len(suffixes)-1:
+            bytes /= 1024.
             i += 1
-        f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+        f = ('%.2f' % bytes).rstrip('0').rstrip('.')
         return '%s %s' % (f, suffixes[i])
 
     @staticmethod
@@ -106,11 +107,12 @@ class Chaturbate(object):
         return True
 
     @staticmethod
-    def run_rtmpdump(info, output, extra_arg=""):
+    def run_rtmpdump(rtmp_info, output_filename, extra_argument=""):
         """Runs rtmpdump with the provided parameters.
 
-        :param info list: A list with all the rtmp info, generated in :func:`get_model_info`.
-        :param output str: Filename where to output the stream.
+        :param list rtmp_info: A list with all the rtmp info, generated in :func:`get_model_info`.
+        :param str output_filename: Filename where to output the stream.
+        :param str extra_argument: Extra argument to pass to rtmpdump.
 
         :return: A Popen object (process).
         :rtype: Popen
@@ -119,43 +121,43 @@ class Chaturbate(object):
             "rtmpdump",
             "--quiet",
             "--live",
-            extra_arg,
-            "--rtmp", "rtmp://" + info[2] + "/live-edge",
-            "--pageUrl", "http://chaturbate.com/" + info[1],
-            "--conn", "S:" + info[8],
-            "--conn", "S:" + info[1],
+            extra_argument,
+            "--rtmp", "rtmp://" + rtmp_info[2] + "/live-edge",
+            "--pageUrl", "http://chaturbate.com/" + rtmp_info[1],
+            "--conn", "S:" + rtmp_info[8],
+            "--conn", "S:" + rtmp_info[1],
             "--conn", "S:2.645",
-            "--conn", "S:" + urllib.unquote(info[15]),
+            "--conn", "S:" + urllib.unquote(rtmp_info[15]),
             "--token", "m9z#$dO0qe34Rxe@sMYxx",
             "--playpath", "playpath",
-            "--flv", output
+            "--flv", output_filename
         ]
 
         return subprocess.Popen(args)
 
     @staticmethod
-    def get_proc_stats(proc):
+    def get_process_stats(process):
         """Generates a dict with various info about the file being captured.
 
-        :param proc dict: A dict with information about a rtmpdump process, generated in :func:`capture`.
+        :param dict process: A dict with information about a rtmpdump process, generated in :func:`capture`.
 
         :return: A dict with statistics about the recording.
         :rtype: dict
         """
-        file_size = int(os.path.getsize(proc['filename']))
+        file_size = int(os.path.getsize(process['filename']))
         return {
             'file_size': file_size,
             'formatted_file_size': Chaturbate.get_human_size(file_size),
             'started_at': time.strftime(
-                "%H:%M", time.localtime(proc['time'])),
+                "%H:%M", time.localtime(process['time'])),
             'recording_time': str(
-                timedelta(seconds=int(time.time()) - proc['time']))
+                timedelta(seconds=int(time.time()) - process['time']))
             }
 
     def make_request(self, url):
         """Does a GET request and returns the HTML content.
 
-        :param url str: The URL to open.
+        :param str url: The URL to open.
 
         :return: The HTML source of the requested URL.
         :rtype: str
@@ -210,18 +212,18 @@ class Chaturbate(object):
 
         return models
 
-    def is_recording(self, model):
+    def is_recording(self, model_name):
         """Checks if a model is already being recorded.
 
         Checks if the parameter is already in the :data:`processes` list.
 
-        :param model str: The model name to check.
+        :param str model_name: The model name to check.
 
         :return: True if successful, False otherwise.
         :rtype: bool
         """
         for process in self.processes:
-            if process['model'] == model and process['type'] == 'rtmpdump':
+            if process['model'] == model_name and process['type'] == 'rtmpdump':
                 return True
 
         return False
@@ -229,7 +231,7 @@ class Chaturbate(object):
     def process_models(self, models):
         """Processes a list that has the online models and starts capturing them.
 
-        :param models list: The models list generated in :func:`get_online_models`.
+        :param list models: The models list generated in :func:`get_online_models`.
         """
         for model in models:
             # already recording it, ignore
@@ -245,15 +247,15 @@ class Chaturbate(object):
                 else:
                     self.logger.warning("But the show is private")
 
-    def get_model_info(self, name):
+    def get_model_info(self, model_name):
         """Generates a list with all EmbedViewerSwf variables from the HTML.
 
-        :param model str: The model name to get info.
+        :param str model_name: The model name to get info.
 
-        :return: A list with all informations for the FLV player.
+        :return: A list with all information from the FLV player.
         :rtype: list
         """
-        url = "https://chaturbate.com/" + name + "/"
+        url = "https://chaturbate.com/" + model_name + "/"
         html = self.make_request(url)
 
         info = []
@@ -270,12 +272,12 @@ class Chaturbate(object):
 
         return info
 
-    def capture(self, info):
+    def capture(self, rtmp_info):
         """Capture a stream.
 
-        Starts a rtmpdump process, and adds information about it to the :data:`processes` list.
+        Starts rtmpdump and adds information about it to the :data:`processes` list.
 
-        :param info list: A list with all the rtmp info, generated in :func:`get_model_info`.
+        :param list rtmp_info : A list with all the rtmp info, generated in :func:`get_model_info`.
         """
         directory = self.config_parser.get('Directories', 'capturing')
 
@@ -284,7 +286,7 @@ class Chaturbate(object):
 
         date_time = datetime.now()
 
-        filename = ("Chaturbate_" + info[1] +
+        filename = ("Chaturbate_" + rtmp_info[1] +
                     date_time.strftime("_%Y-%m-%dT%H%M%S") + ".flv")
 
         message = "Capturing " + filename
@@ -292,19 +294,19 @@ class Chaturbate(object):
 
         filename = os.path.join(directory, filename)
 
-        proc = self.run_rtmpdump(info, filename)
+        proc = self.run_rtmpdump(rtmp_info, filename)
 
         self.processes.append(
             {
-                'id': 'rtmp-' + info[1],
+                'id': 'rtmp-' + rtmp_info[1],
                 'type': 'rtmpdump',
-                'model': info[1],
+                'model': rtmp_info[1],
                 'filename': filename,
                 'time': int(time.time()),
                 'proc': proc,
             })
 
-    def check_running(self):
+    def is_running(self):
         """Processes the :data:`processes` list.
 
         Checks if the rtmpdump is still running, if isn't remove it from list.
@@ -321,7 +323,7 @@ class Chaturbate(object):
                     self.logger.info(
                         proc['model'] + " is no longer being captured")
                     if os.path.isfile(proc['filename']):
-                        proc_stats = self.get_proc_stats(proc)
+                        proc_stats = self.get_process_stats(proc)
                         if proc_stats['file_size'] == 0:
                             self.logger.warning("Capture size is 0kb, deleting.")
                             os.remove(proc['filename'])
@@ -389,7 +391,7 @@ class Chaturbate(object):
         * Gets online models.
         * Process them.
         """
-        c.check_running()
+        c.is_running()
         online_models = self.get_online_models()
         if len(online_models) > 0:
             self.process_models(online_models)
@@ -400,7 +402,7 @@ class Chaturbate(object):
         """
         for proc in self.processes:
             if proc['type'] == 'rtmpdump' and os.path.isfile(proc['filename']):
-                proc_stats = self.get_proc_stats(proc)
+                proc_stats = self.get_process_stats(proc)
                 if proc_stats['file_size'] > 0:
                     message = ("Recording: " +
                                proc['model'] + " - " +
@@ -410,12 +412,12 @@ class Chaturbate(object):
                                proc_stats['formatted_file_size'])
                     self.logger.info(message)
 
-    def is_private(self, info):
+    def is_private(self, rtmp_info):
         """Checks if a stream is private.
 
         Runs rtmpdump for 5 seconds and checks if the file size is > 0.
 
-        :param list info: A list with all the rtmp info, generated in :func:`get_model_info`.
+        :param list rtmp_info: A list with all the rtmp info, generated in :func:`get_model_info`.
 
         :return: True if private, False otherwise.
         :rtype: bool
@@ -424,7 +426,7 @@ class Chaturbate(object):
         seconds = 5
 
         file_name = "test.flv"
-        proc = self.run_rtmpdump(info, file_name, extra_arg="-B " + str(seconds))
+        proc = self.run_rtmpdump(rtmp_info, file_name, extra_argument="-B " + str(seconds))
         proc.wait()
 
         if os.path.isfile(file_name):
@@ -434,39 +436,39 @@ class Chaturbate(object):
 
         return result
 
-    def move_to_complete(self, proc):
+    def move_to_complete(self, process):
         """Moves the recorded file to the completed path.
 
         If ffmpeg postprocessing is enabled, its called after the move.
 
-        :param proc dict: A dict with information about a rtmpdump process, generated in :func:`capture`.
+        :param dict process: A dict with information about a rtmpdump process, generated in :func:`capture`.
         """
         directory = self.config_parser.get('Directories', 'complete')
 
         if os.path.isdir(directory) is False:
             os.mkdir(directory)
 
-        source = proc['filename']
+        source = process['filename']
         flv = source.replace(self.config_parser.get('Directories', 'capturing') + os.sep,
                              self.config_parser.get('Directories', 'complete') + os.sep)
         os.rename(source, flv)
 
         if self.config_parser.get('FFmpeg', 'enable') == "true":
             mp4 = flv.replace(".flv", ".mp4")
-            self.run_ffmpeg(proc['model'], flv, mp4)
+            self.run_ffmpeg(process['model'], flv, mp4)
 
-    def run_ffmpeg(self, model, source, destination):
+    def run_ffmpeg(self, model_name, source_fn, destination_fn):
         """Executes ffmpeg to postprocess recording.
 
-        :param model str: Model name.
-        :param source str: Source file, normally the flv file.
-        :param destination str: Destination file, normally a mp4.
+        :param str model_name: Model name.
+        :param str source_fn: Source file, normally the flv file.
+        :param str destination_fn: Destination file, normally a mp4.
         """
         args = [
             ["nice", "-n", "19"],
-            ['ffmpeg', '-i', source],
+            ['ffmpeg', '-i', source_fn],
             self.config_parser.get('FFmpeg', 'options').split(),
-            [destination]
+            [destination_fn]
             ]
 
         args = [item for sublist in args for item in sublist]
@@ -476,11 +478,11 @@ class Chaturbate(object):
 
         self.processes.append(
             {
-                'id': 'ffmpeg-' + model,
+                'id': 'ffmpeg-' + model_name,
                 'type': 'ffmpeg',
-                'model': model,
-                'source': source,
-                'destination': destination,
+                'model': model_name,
+                'source': source_fn,
+                'destination': destination_fn,
                 'proc': proc,
             })
 
